bf9b2885fdf91c9182f5cfee6c1428e670c87db9
Wccccc <43540548+cjim8889@users.noreply.github.com>
2026-04-05 18:22:44 +0800

    infra: add kubernetes bootstrap module (ingress-nginx, cert-manager)

---
+++ infra/modules/kubernetes/outputs.tf
@@ -0,0 +1,9 @@
+output "load_balancer_ip" {
+  description = "External IP of the ingress-nginx load balancer"
+  value       = data.kubernetes_service.ingress_nginx.status[0].load_balancer[0].ingress[0].ip
+}
+
+output "app_namespace" {
+  description = "Name of the application namespace"
+  value       = kubernetes_namespace.app.metadata[0].name
+}
